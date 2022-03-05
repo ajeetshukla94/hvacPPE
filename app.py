fafb8de672734f80ba87d4c8e4c827ba58da6e6e
Ajeet Shukla <98910114+ajeetshukla94@users.noreply.github.com>
2022-03-06 02:59:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -414,9 +414,9 @@ def submit_updateCompanyDetails():
     temp_df         = pd.DataFrame.from_dict(observation,orient ='index')
     temp_df         = temp_df[['COMPANY_NAME','ADDRESS','REPORT_NUMBER']]
     print(temp_df)
- 
-    fl_path = MYDIR + "/" + app.config['UPLOAD_FOLDER']+"company_details.xlsx"
-    temp_df.to_excel(fl_path,index=False)
+    final_working_directory =MYDIR + "/" + app.config['UPLOAD_FOLDER_INPUTDATA']+"company_details.xlsx"
+
+    temp_df.to_excel(final_working_directory,index=False)
     d = {"error":"none",}
     
     return json.dumps(d)
