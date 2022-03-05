a735d924b04d7cdfb3d30cf1394250ab0f51b759
Ajeet Shukla <98910114+ajeetshukla94@users.noreply.github.com>
2022-03-06 01:48:03 +0530

    update prodcut

--- app.py
+++ app.py
@@ -414,7 +414,7 @@ def submit_updateCompanyDetails():
     temp_df         = pd.DataFrame.from_dict(observation,orient ='index')
     temp_df =temp_df[['COMPANY_NAME','ADDRESS','REPORT_NUMBER']]
  
-    final_working_directory = MYDIR + "static/inputData/company_details.xlsx"
+    final_working_directory = MYDIR + "/"  "static/inputData/company_details.xlsx"
     temp_df.to_excel(final_working_directory,index=False)
     d = {
         "error":"none",
